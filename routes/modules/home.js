f4a8c59c9d9c97e1b60bb91759ac1e644e2dc1aa
Kaikai8888 <wuky812@gmail.com>
2020-11-19 17:49:31 +0800

    feat: add index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
 
-router.get('/', (req, res) => {
-  res.send('hello world')
+router.get('/', async (req, res) => {
+  const todos = await Todo.findAll({ raw: true, nest: true })
+  res.render('index', { todos })
 })
 
 
--- seeders/20201119080357-default-data.js
+++ seeders/20201119080357-default-data.js
@@ -43,6 +43,5 @@ module.exports = {
     } catch (error) {
       console.log(error)
     }
-
   }
 };
