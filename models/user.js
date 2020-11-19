efb9fc1bba94fd94e015eaa97feead227ad45f32
Kaikai8888 <wuky812@gmail.com>
2020-11-19 16:02:04 +0800

    feat: set up User and Todo association

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Todo)
     }
   };
   User.init({
@@ -21,5 +22,6 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
   return User;
 };
